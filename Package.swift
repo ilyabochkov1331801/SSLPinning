6efb3c602baaf1ed2f122119e283302903145cf0
Ilya Bochkov <ilyabochkovib@gmail.com>
2022-03-11 19:38:20 +0300

    no message

--- Package.swift
+++ Package.swift
@@ -1,9 +1,8 @@
 import PackageDescription
 
-let package = Package(name: "SSLPinning",
-                      platforms: [.iOS(.v13)],
-                      products: [.library(name: "SSLPinning",
-                                          targets: ["SSLPinning"])],
-                      targets: [.target(name: "SSLPinning",
-                                        path: "PinningCore/Source"])],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "SSLPinning",
+    platforms: [.iOS(.v13)],
+    products: [.library(name: "SSLPinning", targets: ["SSLPinning"])],
+    targets: [.target(name: "SSLPinning", path: "PinningCore/Source"])]
+)
