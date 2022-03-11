fecc5b77c4c01a9c6661e8f4137e2eeabf1ee53d
Ilya Bochkov <ilyabochkovib@gmail.com>
2022-03-11 19:32:15 +0300

    Add framework and package

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(name: "SSLPinning",
+                      platforms: [.iOS(.v13)],
+                      products: [.library(name: "SSLPinning",
+                                          targets: ["SSLPinning"])],
+                      targets: [.target(name: "SSLPinning",
+                                        path: "PinningCore/Source"])],
+                      swiftLanguageVersions: [.v5])
---
+++ PinningCore/PinningCore.h
@@ -0,0 +1,18 @@
+//
+//  PinningCore.h
+//  PinningCore
+//
+//  Created by Ilya.Bochkov on 11.03.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PinningCore.
+FOUNDATION_EXPORT double PinningCoreVersionNumber;
+
+//! Project version string for PinningCore.
+FOUNDATION_EXPORT const unsigned char PinningCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PinningCore/PublicHeader.h>
+
+
