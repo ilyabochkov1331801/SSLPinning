e07df6202b23c2b775079627218feccf6cc0ce69
Ilya Bochkov <ilyabochkovib@gmail.com>
2022-03-11 19:02:44 +0300

    Initial Commit

---
+++ SSLPinning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSLPinning
+//
+//  Created by Ilya.Bochkov on 11.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
